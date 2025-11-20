0208bd98a9bbb5181ec7de58557563c72b9dac9c
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-20 16:11:16 +0300

    Update models.py

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+
+class Author(models.Model):
+  name = models.CharField(max_length=255)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
