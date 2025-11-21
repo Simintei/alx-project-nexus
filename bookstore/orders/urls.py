9aaa1e0a94acad611ed0b6b22b7401afb6963ddc
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-21 11:23:23 +0300

    Create urls.py

---
+++ bookstore/orders/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views impport CartView, AddToCartView, RemoveFromCartView, CheckOutView
+
+urlpatterns = [
+  path("cart/", CartView.as_view(), name="cart"),
+  path("cart/add/", AddToCartView.as_view(), name="add_to_cart"),
+  path("cart/remove/<int:pk>/", RemoveFromCartView.as_view(), name="remove_from_cart"),
+  path("checkout/", CheckoutView.as_view(), name="checkout"),
+]
