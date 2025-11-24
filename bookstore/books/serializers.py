7884ea600b41ce95208c97dec14b2d3448531815
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-24 10:17:42 +0300

    Update serializers.py

--- bookstore/books/serializers.py
+++ bookstore/books/serializers.py
@@ -1,8 +1,45 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Category, Author
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["id", "name"]
 
 
 class BookSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        source="category",
+        write_only=True
+    )
+
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(),
+        source="author",
+        write_only=True
+    )
+
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = [
+            "id",
+            "title",
+            "author",
+            "author_id",
+            "category",
+            "category_id",
+            "price",
+            "created_at",
+        ]
+
+
