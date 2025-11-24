a3b4c148c4c1c49e1e6c4404ec1390b14f09e52f
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-24 11:57:01 +0300

    Update urls.py

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, re_path, include
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
         description="API for bookstore backend",
     ),
     public=True,
-    permission_classes=[AllowAny],
+    permission_classes=(permissions.AllowAny),
 )
 
 urlpatterns = [
@@ -43,3 +43,15 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+    #Swagger endpoints
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+] 
+
