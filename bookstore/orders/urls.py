b7f4693ce5374eba5d593f5b176c2b62f2f6114e
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-24 11:02:17 +0300

    Update urls.py

--- bookstore/orders/urls.py
+++ bookstore/orders/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views impport CartView, AddToCartView, RemoveFromCartView, CheckOutView
 
+router = DefaultRouter()
+router.register(r'cart', CartItemViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='orders')
+
 urlpatterns = [
   path("cart/", CartView.as_view(), name="cart"),
   path("cart/add/", AddToCartView.as_view(), name="add_to_cart"),
