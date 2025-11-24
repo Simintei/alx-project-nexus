39db7042d8afd616c1e007af9b0d62f3590ec273
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-24 22:38:29 +0300

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import CartView, AddToCartView, RemoveFromCartView, CheckoutView
+from rest_framework.routers import DefaultRouter
+from .views import OrderViewSet, CartView, AddToCartView, RemoveFromCartView, CheckoutView
 
 router = DefaultRouter()
 router.register(r'cart', CartItemViewSet, basename='cart')
