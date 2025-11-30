d23601465fda4964234ed12fc57f9785dc52f7b0
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-30 23:07:56 +0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
