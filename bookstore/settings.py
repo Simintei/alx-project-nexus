1dd14d32a6d25ce02f2740d3502a7b4abf85e295
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-30 20:42:42 +0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django-cors-headers',
     'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',  # fixed typo
