fce8597b11e3ee66838f418cbcc35ac792458695
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-30 20:21:53 +0300

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.shortcuts import redirect
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from users.views import RegisterView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
